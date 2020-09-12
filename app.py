1f5ed9104203e0c387b7acd12ac8249437e5e6c5
Anusha <47362691+anusha-viraj@users.noreply.github.com>
2020-09-12 17:57:36 +0530

    Initial Commit
    Initial Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('modelSalaryPredictor.nair', 'rb'))
+#ohe = pickle.load(open('StateEncoder.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    rdSpend = float(request.form['yearsExp'])
+    state = request.form['state']
+    prediction = model.predict(finalFeatures)
+
+    return render_template('index.html', prediction_text='Expected Profit from the Startup is  $ {}'.format(prediction[0]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
